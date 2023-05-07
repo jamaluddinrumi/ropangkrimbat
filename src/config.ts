c411184e375fb32ef4c4e1d8bafe994e011d9625
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-05-07 18:28:41 +0000

    chore: update some site configs

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://jamal.indomascot.com/",
+  author: "Jamaluddin Rumi",
+  desc: "Website containing life lesson of Jamaluddin Rumi.",
+  title: "RopangKrimbat",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
